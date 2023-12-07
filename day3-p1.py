fedb27fb1c30ecc8202eb6241d4a708e8756c6ce
matt51814 <matthawthorn@hotmail.com>
2023-12-07 23:10:43 +0000

    day3 attempt

--- day3-p1.py
+++ day3-p1.py
@@ -1,50 +1,64 @@
+# think this is almost there, check edge cases. numbers at start and ends of rows
+
 from string import punctuation
 
 with open('day3-input.txt', 'r') as file:
     puzzle_input = file.read()
 
+lines = puzzle_input.split('\n')
+num_indices = []
+for row in range(len(lines)):
+    #print(lines[row])
+    s_idx, e_idx = -1, -1    
+    for col in range(len(lines[row])):
+        elem = lines[row][col]
+        if elem.isdigit() and (col == 0 or lines[row][col-1] == '.' or lines[row][col-1] in punctuation):
+            s_idx = col
+        if (elem == '.' or elem in punctuation) and lines[row][col-1].isdigit():
+            e_idx = col
+        elif col == len(lines[row])-1 and lines[row][col-1].isdigit():
+            e_idx = col + 1
+        if s_idx != -1 and e_idx != -1:
+            #print(lines[row][s_idx:e_idx]) 
+            num_indices.append([row, s_idx, e_idx])   
+            s_idx, e_idx = -1, -1    
 
-full_results_list = []
-line = puzzle_input.split()
-for row in range(1,len(puzzle_input.split())-1):
-    print(row)
-    line_results_list = []
-    for col in range(len(line[1])):
-        if line[row][col] in punctuation and line[row][col] != '.':
-            # print(line[row-1][col-1], line[row-1][col], line[row-1][col+1])
-            # print(line[row][col-1], line[row][col], line[row][col+1])
-            # print(line[row+1][col-1], line[row+1][col], line[row+1][col+1])
+# loop through num indices list, go round surrounding square and if theres a piece of punctuation store the number
+gear_nums = []
+for r, s, e in num_indices:
+    #print(r, s, e)
+    if r == 0:
+        #print(lines[r][s-1:e+1].strip('.'))
+        #print(lines[r+1][s-1:e+1].strip('.'))
+        mid_line = [x for x in lines[r][s-1:e+1].strip('.') if not x.isdigit()]
+        bottom_line = [x for x in lines[r+1][s-1:e+1].strip('.') if not x.isdigit()]
+        if mid_line or bottom_line:
+            print(int(lines[r][s:e]))
+            gear_nums.append(int(lines[r][s:e]))
 
-            for row2 in [-1, 0, 1]:
-                for col2 in [-1, 0, 1]:
-                    if line[row+row2][col+col2].isdigit():
+    elif r == len(lines)-1:
+        # print(lines[r-1][s-1:e+1].strip('.'))
+        # print(lines[r][s-1:e+1].strip('.'))
 
-                        # look right
-                        i = 0
-                        num_string1 = ""
-                        while line[row+row2][col+col2+i].isdigit():
-                            num_string1 += line[1+row2][col+col2+i]
-                            i += 1
-                        #print(num_string1)
+        top_line = [x for x in lines[r-1][s-1:e+1].strip('.') if not x.isdigit()]
+        mid_line = [x for x in lines[r][s-1:e+1].strip('.') if not x.isdigit()]
 
-                        # look left
-                        j = 0
-                        num_string2 = ""
-                        while line[row+row2][col+col2-j].isdigit():
-                            num_string2 += line[row+row2][col+col2-j]
-                            j += 1
-                        #print(num_string2)
-                        num_string2 = num_string2[::-1]
+        if top_line or mid_line:
+            print(int(lines[r][s:e]))
+            gear_nums.append(int(lines[r][s:e]))
 
-                        if len(num_string1) > len(num_string2):
-                            if num_string1 not in line_results_list:
-                                line_results_list.append(num_string1)
-                        elif len(num_string2) > len(num_string1):
-                            if num_string2 not in line_results_list:
-                                line_results_list.append(num_string2)
-                        
-    #print(line_results_list)
-    full_results_list.append(line_results_list)
+    else:
 
-print(full_results_list)
-        
+        top_line = [x for x in lines[r-1][s-1:e+1].strip('.') if not x.isdigit()]
+        mid_line = [x for x in lines[r][s-1:e+1].strip('.') if not x.isdigit()]
+        bottom_line = [x for x in lines[r+1][s-1:e+1].strip('.') if not x.isdigit()]
+
+        if top_line or mid_line or bottom_line:
+            print(int(lines[r][s:e]))
+            gear_nums.append(int(lines[r][s:e]))
+
+        # print(lines[r-1][s-1:e+1].strip('.'))
+        # print(lines[r][s-1:e+1].strip('.'))
+        # print(lines[r+1][s-1:e+1].strip('.'))
+print(gear_nums)
+print(sum(gear_nums))
